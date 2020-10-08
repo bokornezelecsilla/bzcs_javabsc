e349ffa90ab2d90a410ebadca8d279e536ab1cb7
bokornezelecsilla <71121067+bokornezelecsilla@users.noreply.github.com>
2020-10-08 17:41:38 +0200

    V2

--- HtmpApi/src/test/java/hu/training360/javase/htmlapi/HtmlDocTest.java
+++ HtmpApi/src/test/java/hu/training360/javase/htmlapi/HtmlDocTest.java
@@ -5,10 +5,6 @@ import hu.training360.javase.htmlapi.controllers.Button;
 
 public class HtmlDocTest {
 
-    Html root = new Html();
-
-    Button button = new Button("1",  "", "Button", root);
-
-    String html  = root.getHtmlAsString();
+    
 
 }
